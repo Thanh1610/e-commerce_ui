56373824a1af4a65cbabbac5fc3d5a1f5b056283
Thanh1610 <Tatuanthanh2105nd@gmail.com>
2025-05-28 18:11:38 +0700

    install redux-toolkit && styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import type { RootState } from '@/redux/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '@/redux/slides/counterSlide';
+import styled from 'styled-components';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = styled.button({
+    background: 'green',
+});
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App() {
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <div>
+                <Button aria-label="Increment value" onClick={() => dispatch(increment())}>
+                    Increment
+                </Button>
+                <span>{count}</span>
+                <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+                    Decrement
+                </button>
+            </div>
+        </div>
+    );
 }
-
-export default App
