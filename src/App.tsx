2b419c292c9794b800b24dbcb0961a7e65a18554
Thanh1610 <Tatuanthanh2105nd@gmail.com>
2025-06-03 09:13:12 +0700

    Install axios & call backend api

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import { publicRoutes } from './routes';
 import MainLayout from './layouts/MainLayout/MainLayout';
 import { Fragment } from 'react/jsx-runtime';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 export function App() {
+    useEffect(() => {
+        const fetchApi = async () => {
+            const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}product/products`, {
+                params: {
+                    page: 2,
+                    limit: 2,
+                },
+            });
+            console.log('res: ', res.data.data);
+        };
+
+        fetchApi();
+    }, []);
     return (
         <Router>
             <div>
